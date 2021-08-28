be9b9edce25bd5f4a718fc96718740774c5bcae8
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-08-29 05:33:05 +0900

    feat: Types

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, your gender is "${gender}"!`;
+};
+
+console.log(sayHi("Nayeon", 999, "Prefer not to say"));
+
+export {};
